0141230f771613b2fb3ef714db833fc0f67a4853
nhunique <lyhuynhyennhu01@gmail.com>
2025-02-08 17:46:08 -0500

    setup UI components

--- Lab1_Nhu_Ly/App/ContentView.swift
+++ Lab1_Nhu_Ly/App/ContentView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+        @State var count = 0
+        @State var clock = "00:00"
+        
+        @State private var currentNumber: Int = 2
+        @State private var score: Int = 0
+        @State private var timeRemaining = 10
+        @State private var isGameOver = false
+        
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack{
+                Text("\(clock)")
+            }
+            VStack {
+                Text("\(currentNumber)")
+                    .font(.pacifico(fontStyle: .largeTitle))
+                    .foregroundStyle(.black)
+                    .padding()
+                Button {
+
+                } label: {
+                    Text("Prime")
+                        .font(.pacifico(fontStyle: .title))
+                    
+                }
+                Button {
+
+                } label: {
+                    Text("Not Prime")
+                        .font(.pacifico(fontStyle: .title))
+                    
+                }
+            }
+            .padding()
+            VStack{
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .frame(width: 70, height: 70)    .foregroundStyle(.green)
+                Image(systemName: "x.circle")
+                    .resizable()
+                    .frame(width: 70, height: 70)    .foregroundStyle(.red)
+                
+            }
+            Spacer()
+            VStack(alignment: .leading) {
+                Text("\(count)")
+            }
         }
         .padding()
     }
