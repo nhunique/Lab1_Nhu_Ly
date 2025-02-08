fdebd2908e7bf1ae22f65ee50d48997387b2695f
nhunique <lyhuynhyennhu01@gmail.com>
2025-02-08 17:57:17 -0500

    add check prime logic

--- Lab1_Nhu_Ly/App/ContentView.swift
+++ Lab1_Nhu_Ly/App/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
         
-        @State var count = 0
-        @State var clock = "00:00"
-        
-        @State private var currentNumber: Int = 2
-        @State private var score: Int = 0
-        @State private var timeRemaining = 10
-        @State private var isGameOver = false
-        
+    @State var count = 0
+    @State var clock = "00:00"
+    
+    @State private var currentNumber: Int = 2
+    @State private var score: Int = 0
+    @State private var timeRemaining = 10
+    @State private var isGameOver = false
+    
     var body: some View {
         
         VStack {
@@ -29,14 +29,14 @@ struct ContentView: View {
                     .foregroundStyle(.black)
                     .padding()
                 Button {
-
+                    checkAnswer(isPrimeChoice: true)
                 } label: {
                     Text("Prime")
                         .font(.pacifico(fontStyle: .title))
                     
                 }
                 Button {
-
+                    checkAnswer(isPrimeChoice: false)
                 } label: {
                     Text("Not Prime")
                         .font(.pacifico(fontStyle: .title))
@@ -60,6 +60,35 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    // ----- Functions Logic ------ //
+    
+    // Function to generate a new number
+    func generateNewNumber() {
+        currentNumber = Int.random(in: 2...100)
+    }
+    
+    // Function to check if a number is prime
+    func isPrime(_ number: Int) -> Bool {
+        guard number > 1 else {return false}
+        for i in 2..<number {
+            if number % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // Function to validate the answer
+    func checkAnswer(isPrimeChoice: Bool){
+        if isPrime(currentNumber) == isPrimeChoice {
+            score += 1
+        } else {
+            score -= 1
+        }
+        generateNewNumber()
+    }
+    
 }
 
 #Preview {
