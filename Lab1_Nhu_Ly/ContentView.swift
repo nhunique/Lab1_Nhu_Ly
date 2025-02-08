a2f269dd2fe925eaba23e1b38d4924950c0f7cc7
nhunique <lyhuynhyennhu01@gmail.com>
2025-02-08 17:35:43 -0500

    Initial Commit

---
+++ Lab1_Nhu_Ly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Nhu_Ly
+//
+//  Created by Huynh Yen Nhu Ly on 2025-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Nhu_Ly/Lab1_Nhu_LyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Nhu_LyApp.swift
+//  Lab1_Nhu_Ly
+//
+//  Created by Huynh Yen Nhu Ly on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Nhu_LyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
