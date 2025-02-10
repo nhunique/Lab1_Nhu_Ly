79297e97f4ac9254ea7fed3517bf2aa9cb691efc
nhunique <lyhuynhyennhu01@gmail.com>
2025-02-10 18:21:03 -0500

    change timer text style

--- Lab1_Nhu_Ly/App/ContentView.swift
+++ Lab1_Nhu_Ly/App/ContentView.swift
@@ -54,7 +54,8 @@ struct ContentView: View {
                 
                 VStack{
                     Text("\(timeRemaining)")
-                        .font(.headline)
+                        .font(.title)
+                        .foregroundStyle(.red)
                 }
                 Spacer()
                 VStack {
